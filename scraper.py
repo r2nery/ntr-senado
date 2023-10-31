110cca096250821d2f195de447d24c639a6bb1fe
Arthur Nery <99993145+r2nery@users.noreply.github.com>
2023-10-30 23:29:10 -0300

    init

--- scraper.py
+++ scraper.py
@@ -5,44 +5,50 @@ import pandas as pd
 import json
 from alive_progress import alive_bar
 import lxml
+import os
 
-page = 1
 page_size = 1000
-dates = {"10/10/2000": "01/01/2001"}
-dates.update({f"01/01/20{str(i).zfill(2)}": f"01/01/20{str(i+1).zfill(2)}" for i in range(1, 24)})
-dates = tuple(dates.items())
+dates = tuple((f'01/01/{year}', f'31/12/{year}') for year in range(2000, 2024))
+dates = (('10/10/2000', dates[0][1]),) + dates[1:]
 gen_url = (
     lambda page, page_size, data_inicio, data_fim: f"https://www.camara.leg.br/internet/sitaqweb/resultadoPesquisaDiscursos.asp?CurrentPage={page}&BasePesq=plenario&dtInicio={data_inicio}&dtFim={data_fim}&CampoOrdenacao=dtSessao&TipoOrdenacao=ASC&PageSize={page_size}"
 )
 url2 = lambda href: "https://www.camara.leg.br/internet/sitaqweb/" + href
 
-try:
-    df = pd.read_csv("data.csv")
-    df = df.drop_duplicates(subset=["URL_Discurso"])
-    starting_date = df["Data"].iloc[-1][-4:]
-    dates = [date for date in dates if date[0][-4:] >= starting_date]
-except:
-    df = pd.DataFrame(columns=["Data", "Sessão", "Fase", "URL_Discurso", "Discurso", "Orador", "Hora", "Publicação"])
-    df.to_csv("data.csv", index=False)
+# Check for the latest year CSV in directory and continue scraping from the next year
+existing_files = [f for f in os.listdir() if f.endswith('.csv') and f[:-4].isdigit()]
+existing_files.sort(reverse=True)
+
+if existing_files:
+    last_year_scraped = int(existing_files[0].split('.')[0])
+    dates = [date for date in dates if int(date[0][-4:]) > last_year_scraped]
+
+df_columns = ["Data", "Sessão", "Fase", "URL_Discurso", "Discurso", "Orador", "Hora", "Publicação"]
 
 for data_inicio, data_fim in dates:
-    url = gen_url(page, page_size, data_inicio, data_fim)
+    year_data = []
+    url = gen_url(1, page_size, data_inicio, data_fim)
     response = requests.get(url)
     response.encoding = "utf-8"
     soup = BeautifulSoup(response.text, "lxml")
     vstr = soup.find_all("span", {"class": "visualStrong"})
     num_citations = int(vstr[-1].text.replace(".", ""))
     num_pages = num_citations // page_size + 2
-    with alive_bar(num_citations) as bar:
+    with alive_bar(total=num_citations, title=f"Scraping year {data_inicio[-4:]}") as bar:
         for page in range(1, num_pages):
-            url = gen_url(page, page_size, data_inicio, data_fim)
-            response = requests.get(url)
-            response.encoding = "utf-8"
-            soup = BeautifulSoup(response.text, "lxml")
-            table = soup.find("table", {"class": "table table-bordered variasColunas"})
-            rows = table.find_all("tr", {"class": "even"})
-            rows += table.find_all("tr", {"class": "odd"})
-            rows = [row for row in rows if not row.get("id")]
+            try:
+                url = gen_url(page, page_size, data_inicio, data_fim)
+                response = requests.get(url)
+                response.encoding = "utf-8"
+                soup = BeautifulSoup(response.text, "lxml")
+                table = soup.find("table", {"class": "table table-bordered variasColunas"})
+                rows = table.find_all("tr", {"class": "even"})
+                rows += table.find_all("tr", {"class": "odd"})
+                rows = [row for row in rows if not row.get("id")]
+            except Exception as e:
+                # this exception handles the case where the page has no table
+                print(f"Exception: {e}")
+                continue
             for row in rows:
                 cols_data = []
                 eles = row.find_all("td")
@@ -61,9 +67,9 @@ for data_inicio, data_fim in dates:
                             cols_data.append(discurso.strip())
                     else:
                         cols_data.append(ele.text.strip())
-                df = pd.DataFrame(
-                    [cols_data],
-                    columns=["Data", "Sessão", "Fase", "URL_Discurso", "Discurso", "Orador", "Hora", "Publicação"],
-                )
-                df.to_csv("data.csv", mode="a", header=False, index=False)
+                year_data.append(cols_data)
                 bar()
+    
+    # Save the year data to a CSV after scraping the entire year
+    df_year = pd.DataFrame(year_data, columns=df_columns)
+    df_year.to_csv(f"{data_inicio[-4:]}.csv", index=False)
